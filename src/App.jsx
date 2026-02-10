8481a4014b382d9d4696521f5452295047fc5081
Tony <tonyguarino1489@gmail.com>
2026-02-09 21:37:45 -0500

    Added styles for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-const Card = ({title}) => {
+const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
-    <div>
+    <div className="card">
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(true)}>Like</button>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   return (
     <div className="card-container">
       <h2>Tony Guarino</h2>
-      <Card title="Superman"/>
-      <Card title="Avatar"/>
-      <Card title="Avengers"/>
-      <Card title="The Matrix"/>
+      <Card title="Superman" />
+      <Card title="Avatar" />
+      <Card title="Avengers" />
+      <Card title="The Matrix" />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
